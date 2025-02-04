231fa6818b016b91fb56384d193207c4af76cb8c
cloudgoodies <cloudgoodies2345@gmail.com>
2025-02-04 21:36:02 +0530

    Update GSP1145.sh

--- Tagging Dataplex Assets/GSP1145.sh
+++ Tagging Dataplex Assets/GSP1145.sh
@@ -27,6 +27,7 @@ RESET=$(tput sgr0)
 echo "${BG_BLUE}${WHITE}${BOLD} Starting Execution ${RESET}"
 
 gcloud services enable dataplex.googleapis.com
+
 gcloud services enable datacatalog.googleapis.com
 
 gcloud dataplex lakes create orders-lake \
@@ -43,19 +44,15 @@ gcloud dataplex zones create customer-curated-zone \
     --discovery-schedule="0 * * * *"
 
 gcloud dataplex assets create customer-details-dataset \
-  --location=$REGION \
-  --lake=orders-lake \
-  --zone=customer-curated-zone \
-  --display-name="Customer Details Dataset" \
-  --resource-type=BIGQUERY_DATASET \
-  --resource-name=projects/$DEVSHELL_PROJECT_ID/datasets/customers \
-  --discovery-enabled
+--location=$REGION \
+--lake=orders-lake \
+--zone=customer-curated-zone \
+--display-name="Customer Details Dataset" \
+--resource-type=BIGQUERY_DATASET \
+--resource-name=projects/$DEVSHELL_PROJECT_ID/datasets/customers \
+--discovery-enabled
 
-gcloud data-catalog tag-templates create protected_data_template \
-  --location=$REGION \
-  --field=id=protected_data_flag,display-name="Protected Data Flag",type='enum(YES|NO)' \
-  --display-name="Protected Data Template"
+gcloud data-catalog tag-templates create protected_data_template --location=$REGION --field=id=protected_data_flag,display-name="Protected Data Flag",type='enum(YES|NO)' --display-name="Protected Data Template"
 
-echo -e "${CYAN}${BOLD}Click here: ${RESET}${YELLOW}${BOLD}https://console.cloud.google.com/dataplex/search?project=$DEVSHELL_PROJECT_ID&qSystems=DATAPLEX${RESET}"
-
+echo "${CYAN}${BOLD}Click here: "${RESET}""${BLUE}${BOLD}"https://console.cloud.google.com/dataplex/search?project=$DEVSHELL_PROJECT_ID&qSystems=DATAPLEX""${RESET}"
 #-----------------------------------------------------end----------------------------------------------------------#
