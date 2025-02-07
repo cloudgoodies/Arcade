255081bb42761353f879da2170d161f64073c3cb
cloudgoodies <cloudgoodies2345@gmail.com>
2025-02-07 15:56:01 +0530

    Create arc1204.sh

---
+++ Arcade Hero: Building Blocks PubSub I/arc1204.sh
@@ -0,0 +1,3 @@
+echo -e "\e[32mgcloud pubsub topics create sports_topic\e[0m"
+echo -e "\e[34mgcloud pubsub topics create app_topic\e[0m"
+echo -e "\e[33mgcloud pubsub subscriptions create app_subscription --topic=app_topic\e[0m"
