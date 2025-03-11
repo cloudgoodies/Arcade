f3c1296cc3c31a47db40c9166fb041cb106b3bd7
cloudgoodies <cloudgoodies2345@gmail.com>
2025-03-11 15:48:18 +0530

    Update GSP406.sh

--- Using BigQuery in the Google Cloud Console/GSP406.sh
+++ Using BigQuery in the Google Cloud Console/GSP406.sh
@@ -1,25 +1,36 @@
-# Query the top 10 most popular names from the public dataset
-bq query --nouse_legacy_sql \
+
+
+
+bq query --use_legacy_sql=false \
 "
-SELECT name, gender, SUM(number) AS total
-FROM \`bigquery-public-data.usa_names.usa_1910_2013\`
-GROUP BY name, gender
-ORDER BY total DESC
-LIMIT 10
+SELECT
+  name, gender,
+  SUM(number) AS total
+FROM
+  \`bigquery-public-data.usa_names.usa_1910_2013\`
+GROUP BY
+  name, gender
+ORDER BY
+  total DESC
+LIMIT
+  10
 "
 
-# Create the dataset only if it doesn’t already exist
-bq --quiet mk --dataset IF NOT EXISTS babynames
+bq mk babynames
 
-# Create the table with the specified schema
-bq mk --table --schema "name:STRING,count:INTEGER,gender:STRING" babynames.names_2014
 
-# Query the top 5 most popular male names from the names_2014 table
-bq query --nouse_legacy_sql \
+bq mk --table \
+  --schema "name:string,count:integer,gender:string" \
+  $DEVSHELL_PROJECT_ID:babynames.names_2014
+
+
+bq query --use_legacy_sql=false \
 "
-SELECT name, count
-FROM \`babynames.names_2014\`
-WHERE gender = 'M'
-ORDER BY count DESC
-LIMIT 5
-"
+SELECT
+ name, count
+FROM
+ \`babynames.names_2014\`
+WHERE
+ gender = 'M'
+ORDER BY count DESC LIMIT 5
+" 
