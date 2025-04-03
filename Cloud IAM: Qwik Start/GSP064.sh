9142f81afbb624a687e9673095636b62eae1e010
cloudgoodies <cloudgoodies2345@gmail.com>
2025-04-03 21:02:23 +0530

    Create GSP064.sh

---
+++ Cloud IAM: Qwik Start/GSP064.sh
@@ -0,0 +1,35 @@
+# Define color codes for output formatting
+YELLOW_COLOR=$'\033[0;33m'
+NO_COLOR=$'\033[0m'
+BACKGROUND_RED=`tput setab 1`
+GREEN_TEXT=`tput setab 2`
+RED_TEXT=`tput setaf 1`
+
+BOLD_TEXT=`tput bold`
+RESET_FORMAT=`tput sgr0`
+
+echo "${BACKGROUND_RED}${BOLD_TEXT}Starting Execution...${RESET_FORMAT}"
+
+# Prompt user for their username
+read -p "${YELLOW_COLOR}Enter USERNAME 2: ${NO_COLOR}" USERNAME_2
+
+# Set the storage bucket and enable uniform bucket-level access
+gsutil mb -l us -b on gs://$DEVSHELL_PROJECT_ID
+
+
+# Upload the sample file to the specified bucket
+gsutil cp sample.txt gs://$DEVSHELL_PROJECT_ID
+
+# Remove IAM policy binding for the specified user
+gcloud projects remove-iam-policy-binding $DEVSHELL_PROJECT_ID \
+  --member=user:$USERNAME_2 \
+  --role=roles/viewer
+
+# Add IAM policy binding for storage object viewer role
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+  --member=user:$USERNAME_2 \
+  --role=roles/storage.objectViewer
+
+
+# Completion message
+echo -e "${GREEN_TEXT}${BOLD_TEXT}Congratulation Lab Completed Successfully!${RESET_FORMAT}"
