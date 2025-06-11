ae4c3753881a924b94fb71f0e35a7e1d0d6e0978
cloudgoodies <cloudgoodies2345@gmail.com>
2025-06-11 21:55:22 +0530

    Create variables.tf

---
+++ HTTPS Content-Based Load Balancer with Terraform/variables.tf
@@ -0,0 +1,21 @@
+
+
+variable "group1_region" {
+  default = "REGION1"
+}
+
+variable "group2_region" {
+  default = "REGION2"
+}
+
+variable "group3_region" {
+  default = "REGION3"
+}
+
+variable "network_name" {
+  default = "ml-bk-ml-mig-bkt-s-lb"
+}
+
+variable "project" {
+  type = string
+}
