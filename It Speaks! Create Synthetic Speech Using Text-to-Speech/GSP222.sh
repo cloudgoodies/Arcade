c231b9a21d148e148f9e35de98585ffaa231fd24
cloudgoodies <cloudgoodies2345@gmail.com>
2025-06-16 19:19:56 +0530

    Update GSP222.sh

--- It Speaks! Create Synthetic Speech Using Text-to-Speech/GSP222.sh
+++ It Speaks! Create Synthetic Speech Using Text-to-Speech/GSP222.sh
@@ -17,5 +17,3 @@ gcloud iam service-accounts create tts-qwiklab
 gcloud iam service-accounts keys create tts-qwiklab.json --iam-account tts-qwiklab@$PROJECT_ID.iam.gserviceaccount.com
 
 export GOOGLE_APPLICATION_CREDENTIALS=tts-qwiklab.json
-
-
