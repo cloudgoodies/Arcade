9104868ccfece57263d85006417fc9798689d4e2
cloudgoodies <cloudgoodies2345@gmail.com>
2025-06-20 19:07:13 +0530

    Create GSP483.sh

---
+++ Cloud Logging on Kubernetes Engine/GSP483.sh
@@ -0,0 +1,51 @@
+
+gcloud auth list
+
+export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+
+export PROJECT_ID=$(gcloud config get-value project)
+gcloud config set project $DEVSHELL_PROJECT_ID
+
+gcloud config set compute/zone "$ZONE"
+gcloud config set compute/region "$REGION"
+
+git clone https://github.com/GoogleCloudPlatform/gke-logging-sinks-demo
+
+sleep 10
+
+cd gke-logging-sinks-demo
+
+sleep 10
+
+sed -i 's/  version = "~> 2.11.0"/  version = "~> 2.19.0"/g' terraform/provider.tf
+
+sed -i 's/  filter      = "resource.type = container"/  filter      = "resource.type = k8s_container"/g' terraform/main.tf
+
+make create
+make validate
+
+gcloud logging read "resource.type=k8s_container AND resource.labels.cluster_name=stackdriver-logging" --project=$PROJECT_ID
+
+# run the specific query
+gcloud logging read "resource.type=k8s_container AND resource.labels.cluster_name=stackdriver-logging" --project=$PROJECT_ID --format=json
+
+
+
+gcloud logging sinks create techcps \
+    bigquery.googleapis.com/projects/$PROJECT_ID/datasets/bq_logs \
+    --log-filter='resource.type="k8s_container" 
+resource.labels.cluster_name="stackdriver-logging"' \
+    --include-children \
+    --format='json'
+
+
+sleep 20
+
+
+
+bq query --use_legacy_sql=false \
+"
+SELECT * FROM \`$DEVSHELL_PROJECT_ID.gke_logs_dataset.diagnostic_log_*\`
+WHERE _TABLE_SUFFIX BETWEEN FORMAT_DATE('%Y%m%d', CURRENT_DATE() - INTERVAL 1 DAY) AND FORMAT_DATE('%Y%m%d', CURRENT_DATE()) 
+"
