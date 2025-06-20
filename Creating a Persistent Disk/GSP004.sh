faba5a9bd6536245c161cb453bec12b4de44e541
cloudgoodies <cloudgoodies2345@gmail.com>
2025-06-20 20:37:15 +0530

    Update GSP004.sh

--- Creating a Persistent Disk/GSP004.sh
+++ Creating a Persistent Disk/GSP004.sh
@@ -1,31 +1,56 @@
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
 
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
 
-gcloud auth list
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
 
-export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+echo "${BG_MAGENTA}${BOLD}  Starting Execution ...${RESET}"
 
-export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+export REGION="${ZONE%-*}"
 
-export PROJECT_ID=$(gcloud config get-value project)
-
-gcloud config set compute/zone "$ZONE"
-
-gcloud config set compute/region "$REGION"
+gcloud config set compute/zone $ZONE
+gcloud config set compute/region $REGION
 
 gcloud compute instances create gcelab --zone $ZONE --machine-type e2-standard-2
-
 
 gcloud compute disks create mydisk --size=200GB \
 --zone $ZONE
 
 gcloud compute instances attach-disk gcelab --disk mydisk --zone $ZONE
 
-gcloud compute instances create gcelab2 --machine-type e2-medium --zone=$ZONE
+cat > prepare_disk.sh <<'EOF_END'
 
+ls -l /dev/disk/by-id/
 
-gcloud compute ssh gcelab --zone $ZONE --quiet --command "sudo mkdir /mnt/mydisk &&
-  sudo mkfs.ext4 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/disk/by-id/scsi-0Google_PersistentDisk_persistent-disk-1 &&
-  sudo mount -o discard,defaults /dev/disk/by-id/scsi-0Google_PersistentDisk_persistent-disk-1 /mnt/mydisk &&
-  echo '/dev/disk/by-id/scsi-0Google_PersistentDisk_persistent-disk-1 /mnt/mydisk ext4 defaults 1 1' | sudo tee -a /etc/fstab"
+sudo mkdir /mnt/mydisk
 
-  
+sudo mkfs.ext4 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/disk/by-id/scsi-0Google_PersistentDisk_persistent-disk-1
+
+sudo mount -o discard,defaults /dev/disk/by-id/scsi-0Google_PersistentDisk_persistent-disk-1 /mnt/mydisk
+
+EOF_END
+
+gcloud compute scp prepare_disk.sh gcelab:/tmp --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet
+
+gcloud compute ssh gcelab --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet --command="bash /tmp/prepare_disk.sh"
+
+echo
+echo "${GREEN}${BOLD} Congratulations For Completing The Lab !!! ${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
